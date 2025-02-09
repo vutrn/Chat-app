b393e55686f9cd0615d3396bb727337405ff1879
vutrn <meoiuwai@gmail.com>
2025-02-09 16:04:29 +0700

    add sendMessage functionality with image upload and update message routes

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
 
@@ -6,6 +7,7 @@ const getUsersForSidebar = async (req, res) => {
     const loggedInUserId = req.user._id;
     const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
     res.status(200).json(filteredUsers);
+    // console.log(filteredUsers);
   } catch (error) {
     console.log("Error in getting users for sidebar:", error.message);
     res.status(500).json({ message: "Server error" });
@@ -32,7 +34,33 @@ const getMessages = async (req, res) => {
 };
 
 const sendMessage = async (req, res) => {
-  
-}
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
 
-export { getUsersForSidebar, getMessages };
+    let imageUrl = "";
+    if (image) {
+      // Upload base64 image to cloudinary
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+    await newMessage.save();
+
+    // todo: real-time message sending using socket.io
+
+    res.status(200).json(newMessage);
+  } catch (error) {
+    console.log("Error in sending message:", error.message);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export { getUsersForSidebar, getMessages, sendMessage };
