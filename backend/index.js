539046bf2015ef5da8956f6f13394a35fba8a92b
vutrn <meoiuwai@gmail.com>
2025-02-11 02:47:27 +0700

    feat: configure production build and static file serving; update axios base URL for environment

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,12 @@ import { connectDB } from "./src/lib/db.js";
 import { app, server } from "./src/lib/socket.js";
 import authRoutes from "./src/routes/auth.route.js";
 import messageRoutes from "./src/routes/message.route.js";
+import path from "path";
 
 dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(cookieParser());
@@ -16,7 +20,13 @@ app.use(express.json({ limit: "1mb" }));
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-const PORT = process.env.PORT || 5000;
+app.use(express.static(path.join(__dirname, "public")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT} `);
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,10 +3,10 @@ import { io } from "socket.io-client";
 import { create } from "zustand";
 import axiosInstance from "../lib/axios";
 
-const BASE_URL = "http://localhost:5001";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
 
 export const useAuthStore = create((set, get) => ({
-  authUser: null,
+  authUser: null, 
   isSigningUp: false,
   isLoggingIn: false,
   isUpdatingProfile: false,
